a0320fd8b899cdecb722674fd75cdb1925e4fb5c
BELNICHEL <ABHNVKAPIL542@GMAIL.COM>
2024-07-01 13:08:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ DT_model = joblib.load("DT_model1.pkl")
 XGB_model = joblib.load("XGB_model1.pkl")
 RF_model = joblib.load("RF_model1.pkl")
 SVR_model = joblib.load("SVM_model1.pkl")
-ANN_model = joblib.load("ANN_model1.pkl")
 scaler = joblib.load("scaler1.pkl")
 
 # Define parameter names
@@ -45,14 +44,12 @@ def predict():
     selected_model = request.form['model']
     if selected_model == "Random Forest":
         model = RF_model
-    elif selected_model == "XGBoost":
+    elif selected_model == "Extreme Gradient Boosting":
         model = XGB_model
     elif selected_model == "Decision Tree":
         model = DT_model
-    elif selected_model == "SVR":
+    elif selected_model == "Support Vector Regression":
         model = SVR_model
-    elif selected_model == "ANN":
-        model = ANN_model
     
     # Perform prediction using the selected model
     prediction = model.predict(scaled_params)[0]
